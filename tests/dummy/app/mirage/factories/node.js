acbf11e52edbebfcd005859837ef374715fff128
Taras Mankovski <tarasm@gmail.com>
2015-10-06 11:10:59 -0400

    Added paginated example

---
+++ tests/dummy/app/mirage/factories/node.js
@@ -0,0 +1,12 @@
+/*
+  This is an example factory definition.
+
+  Create more files in this directory to define additional factories.
+*/
+import Mirage/*, {faker} */ from 'ember-cli-mirage';
+
+export default Mirage.Factory.extend({
+  name(id) {
+    return `Node ${id}`;
+  }
+});
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('simple');
+  this.route('paginated');
 });
 
 export default Router;
