a0996236ce80cf39227e21a370224a4c14d9df18
Taras Mankovski <tarasm@gmail.com>
2015-12-09 16:29:21 -0500

    Added new functions to page object

--- addon/tests/page-object.js
+++ addon/tests/page-object.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
+import indexOf from 'lodash/array/findIndex'
+
 
 export default class AsyncTreePageObject {
   constructor(env) {
@@ -14,27 +16,43 @@ export default class AsyncTreePageObject {
     return Ember.$(items[index]).text().trim();
   }
 
+  itemsText(selector) {
+    let results = this.find(selector).toArray();
+    return results.map(function(item){
+      return Ember.$(item).text().trim();
+    });
+  }
+
   itemContains(text) {
     let items = sortItemsByPosition(this.env, true);
     return items.find(`.node-list-item:contains('${text}')`);
   }
+
+  find(selector) {
+    return sortItemsByPosition(this.env, true, selector);
+  }
+
+  findIndex(selector, predicate) {
+    let results = this.find(selector).toArray();
+    return indexOf(results, predicate);
+  }
 }
 
-function findItems(context) {
-  return context.$('.ember-collection > div:first > div:first > div');  // scrollable's content's children (cells)
+function findItems(context, selector) {
+  return context.$(`.ember-collection > div:first > div:first > div ${selector}`);  // scrollable's content's children (cells)
 }
 
-function findVisibleItems(context) {
-  return context.$('.ember-collection > div:first > div:first > div:visible');
+function findVisibleItems(context, selector) {
+  return context.$(`.ember-collection > div:first > div:first > div:visible ${selector}`);
 }
 
 function extractPosition(element) {
   return element.getBoundingClientRect();
 }
 
-function sortItemsByPosition(view, visibleOnly) {
+function sortItemsByPosition(view, visibleOnly, selector) {
   var find = visibleOnly ? findVisibleItems : findItems;
-  var items = find(view);
+  var items = find(view, selector);
   return sortElementsByPosition(items);
 }
 
