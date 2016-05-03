5309571b87d3f6aec7a11fe860963c59e647f290
Dale Ma <dalema22@gmail.com>
2016-05-03 16:28:32 -0700

    naming

--- addon/components/async-tree.js
+++ addon/components/async-tree.js
@@ -17,7 +17,7 @@ export default Component.extend(ResizeAware, {
   resizeDebouncedEventsEnabled: false,
   classNameBindings: [':async-tree', 'isLoading'],
 
-  autoIndentation: true,
+  autoIndent: true,
   indentation: 20,
 
   checkInitialChanged() {
